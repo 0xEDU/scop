54d176d6d5c39da1a96e6a3bc0d7bece4837f02e
etachott <bfwaend@gmail.com>
2024-03-23 18:08:17 -0300

    Add mlx_new_image port

--- src/lib.rs
+++ src/lib.rs
@@ -9,9 +9,39 @@ pub struct MlxT {
     delta_time: f64,
 }
 
+#[repr(C)]
+pub struct MlxInstanceT {
+    x: i32,
+    y: i32,
+    z: i32,
+    enabled: bool,
+}
+
+// typedef struct mlx_image
+// {
+// 	const uint32_t	width;
+// 	const uint32_t	height;
+// 	uint8_t*		pixels;
+// 	mlx_instance_t*	instances;
+// 	size_t			count;
+// 	bool			enabled;
+// 	void*			context;
+// }	mlx_image_t;
+#[repr(C)]
+pub struct MlxImageT {
+    width: i32,
+    height: i32,
+    pixels: *mut i8,
+    instances: *mut MlxInstanceT,
+    count: usize,
+    enabled: bool,
+    context: *mut c_void,
+}
+
 extern "C" {
     pub fn mlx_init(width: i32, height: i32, title: *const c_char, resize: bool) -> *mut MlxT;
     pub fn mlx_loop(mlx: *mut MlxT) -> c_void;
     pub fn mlx_terminate(mlx: *mut MlxT) -> c_void;
+    pub fn mlx_new_image(mlx: *mut MlxT, width: i32, height: i32) -> *mut MlxImageT;
 }
 
