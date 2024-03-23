81243bb1fa014cc8494923a7f150c2c3172c9374
etachott <bfwaend@gmail.com>
2024-03-23 17:56:01 -0300

    Move mlx lib to mlx folder

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("cargo:rustc-link-search=native=./libs");
+    println!("cargo:rustc-link-search=native=./mlx");
     println!("cargo:rustc-link-lib=static=mlx42");
 
     // Link the additional libraries
