0db4f0c52825f8f6443452c539eae12506278e99
etachott <bfwaend@gmail.com>
2024-03-23 17:58:50 -0300

    Move mlx port to lib.rs file

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-use libc::{c_char, c_void};
-
-#[repr(C)]
-pub struct MlxT {
-    window: *mut c_void,
-    context: *mut c_void,
-    width: i32,
-    height: i32,
-    delta_time: f64,
-}
-
-extern "C" {
-    pub fn mlx_init(width: i32, height: i32, title: *const c_char, resize: bool) -> *mut MlxT;
-    pub fn mlx_loop(mlx: *mut MlxT) -> c_void;
-    pub fn mlx_terminate(mlx: *mut MlxT) -> c_void;
-}
+use scop::*;
+use libc::c_char;
 
 fn main() {
     let window_name: &str = "dale";
