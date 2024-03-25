7984ee2aaaeaa8c63076f575f7f046059d05f2dc
etachott <bfwaend@gmail.com>
2024-03-25 19:54:44 -0300

    We have a triangle, yay!

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,188 @@
 extern crate glfw;
 
-use glfw::{Action, Context, Key};
+use std::{
+    ffi::CString,
+    ptr::{self},
+};
+use std::{mem, str};
+
+use gl::types::{GLchar, GLfloat, GLint, GLsizei, GLsizeiptr};
+use glfw::{Action, Context, GlfwReceiver, Key, WindowEvent};
+
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 600;
+
+const VERTEX_SHADER_SOURCE: &str = r#"
+    #version 330 core
+    layout (location = 0) in vec3 aPos;
+
+    void main() {
+        gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    }
+"#;
+
+const FRAGMENT_SHADER_SOURCE: &str = r#"
+    #version 330 core
+    out vec4 FragColor;
+
+    void main() {
+        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
+    }
+"#;
 
 fn main() {
+    // Init OpenGL
     let mut glfw = glfw::init(glfw::fail_on_errors).unwrap();
 
-    let (mut window, events) = glfw.create_window(300, 300, "Hello this is window", glfw::WindowMode::Windowed)
-        .expect("Failed to create GLFW window.");
+    // Window options
+    glfw.window_hint(glfw::WindowHint::ContextVersionMajor(3));
+    glfw.window_hint(glfw::WindowHint::ContextVersionMinor(3));
+    glfw.window_hint(glfw::WindowHint::OpenGlProfile(
+        glfw::OpenGlProfileHint::Core,
+    ));
 
+    // Create a window
+    let (mut window, events) = glfw
+        .create_window(
+            WINDOW_WIDTH,
+            WINDOW_HEIGHT,
+            "Hello this is window",
+            glfw::WindowMode::Windowed,
+        )
+        .expect("Failed to create GLFW window.");
     window.set_key_polling(true);
+    window.set_framebuffer_size_polling(true);
+    window.set_framebuffer_size_callback(|_, width, height| unsafe {
+        gl::Viewport(0, 0, width, height)
+    });
     window.make_current();
 
-    while !window.should_close() {
-        glfw.poll_events();
-        for (_, event) in glfw::flush_messages(&events) {
-            handle_window_event(&mut window, event);
+    gl::load_with(|symbol| window.get_proc_address(symbol) as *const _);
+
+    let vertices: [f32; 9] = [-0.5, -0.5, 0.0, 0.5, -0.5, 0.0, 0.0, 0.5, 0.0];
+
+    // We need to write manually at least 2 shaders: vertex shader and fragment shader
+    let (shader_program, vao) = unsafe {
+        // Vertex shader
+        let vertex_shader = gl::CreateShader(gl::VERTEX_SHADER);
+        let c_str_vert = CString::new(VERTEX_SHADER_SOURCE.as_bytes()).unwrap();
+        gl::ShaderSource(vertex_shader, 1, &c_str_vert.as_ptr(), ptr::null());
+        gl::CompileShader(vertex_shader);
+
+        // Check for compilation errors
+        let mut success = gl::FALSE as GLint;
+        let mut info_log = Vec::with_capacity(512);
+        info_log.set_len(512 - 1);
+        gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
+        if success != gl::TRUE as GLint {
+            gl::GetShaderInfoLog(
+                vertex_shader,
+                512,
+                ptr::null_mut(),
+                info_log.as_mut_ptr() as *mut GLchar,
+            );
+            println!(
+                "ERROR COMPILATION FAILED!\n{}",
+                str::from_utf8(&info_log).unwrap()
+            );
         }
+
+        // Fragment shader
+        let fragment_shader = gl::CreateShader(gl::FRAGMENT_SHADER);
+        let c_str_vert = CString::new(FRAGMENT_SHADER_SOURCE.as_bytes()).unwrap();
+        gl::ShaderSource(fragment_shader, 1, &c_str_vert.as_ptr(), ptr::null());
+        gl::CompileShader(fragment_shader);
+        gl::GetShaderiv(vertex_shader, gl::COMPILE_STATUS, &mut success);
+        if success != gl::TRUE as GLint {
+            gl::GetShaderInfoLog(
+                vertex_shader,
+                512,
+                ptr::null_mut(),
+                info_log.as_mut_ptr() as *mut GLchar,
+            );
+            println!(
+                "ERROR COMPILATION FAILED!\n{}",
+                str::from_utf8(&info_log).unwrap()
+            );
+        }
+
+        // Link shaders
+        let shader_program = gl::CreateProgram();
+        gl::AttachShader(shader_program, vertex_shader);
+        gl::AttachShader(shader_program, fragment_shader);
+        gl::LinkProgram(shader_program);
+        gl::GetProgramiv(shader_program, gl::COMPILE_STATUS, &mut success);
+        if success != gl::TRUE as GLint {
+            gl::GetShaderInfoLog(
+                vertex_shader,
+                512,
+                ptr::null_mut(),
+                info_log.as_mut_ptr() as *mut GLchar,
+            );
+            println!(
+                "ERROR LINKING FAILED!\n{}",
+                str::from_utf8(&info_log).unwrap()
+            );
+        }
+        gl::DeleteShader(vertex_shader);
+        gl::DeleteShader(fragment_shader);
+
+        // Load vertex data
+        let (mut vbo, mut vao) = (0, 0);
+        gl::GenVertexArrays(1, &mut vao);
+        gl::GenBuffers(1, &mut vbo);
+        gl::BindVertexArray(vao);
+        gl::BindBuffer(gl::ARRAY_BUFFER, vbo);
+        gl::BufferData(
+            gl::ARRAY_BUFFER,
+            (vertices.len() * std::mem::size_of::<GLfloat>()) as GLsizeiptr,
+            &vertices[0] as *const f32 as *const std::os::raw::c_void,
+            gl::STATIC_DRAW,
+        );
+        gl::VertexAttribPointer(
+            0,
+            3,
+            gl::FLOAT,
+            gl::FALSE,
+            3 * mem::size_of::<GLfloat>() as GLsizei,
+            ptr::null(),
+        );
+        gl::EnableVertexAttribArray(0);
+        gl::BindBuffer(gl::ARRAY_BUFFER, 0);
+        gl::BindVertexArray(0);
+
+        // Draw wireframe polygons
+        // gl::PolygonMode(gl::FRONT_AND_BACK, gl::LINE);
+
+        (shader_program, vao)
+    };
+
+    while !window.should_close() {
+        handle_window_events(&mut window, &events);
+        unsafe {
+            gl::ClearColor(0.2 as f32, 0.3 as f32, 0.3 as f32, 1.0 as f32);
+            gl::Clear(gl::COLOR_BUFFER_BIT);
+
+            // draw triangle, finally
+            gl::UseProgram(shader_program);
+            gl::BindVertexArray(vao);
+            gl::DrawArrays(gl::TRIANGLES, 0, 3);
+        }
+        window.swap_buffers();
+        glfw.poll_events();
     }
 }
 
-fn handle_window_event(window: &mut glfw::Window, event: glfw::WindowEvent) {
-    match event {
-        glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
-            window.set_should_close(true)
+fn handle_window_events(window: &mut glfw::Window, events: &GlfwReceiver<(f64, WindowEvent)>) {
+    for (_, event) in glfw::flush_messages(events) {
+        match event {
+            glfw::WindowEvent::FramebufferSize(width, height) => unsafe {
+                gl::Viewport(0, 0, width, height)
+            },
+            glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
+                window.set_should_close(true)
+            }
+            _ => {}
         }
-        _ => {}
     }
 }
